b6909a8960d67f603e710e5747ae1e55c47fdb44
mateuszmendrek <mateuszmendrek99@gmail.com>
2025-11-18 09:09:27 +0100

    Delete category_profitability.sql

--- category_profitability.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    Category,
-    SUM(Sales) AS TotalSales,
-    SUM(Profit) AS TotalProfit,
-    AVG(Discount) AS AvgDiscount
-FROM superstore
-GROUP BY Category
-ORDER BY TotalProfit DESC;
